e8965b81d45e600d4c1ee3e9fb78d8a1e3b350cf
andritianaa <andritianasteve@gmail.com>
2022-01-11 22:21:18 +0300

    auto take picture when face found and crop face

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
+const faceapi = require("face-api.js");
 const path = require('path');
+const canvas = require("canvas");
+const fs = require("fs");
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 const basePath = path.join(__dirname, '../public')
+app.use(express.static(basePath))
 
 app.listen(port, () => {
     console.log("server started on port "+port);
-} )
+})
+
+
+
 //<>
+
+
