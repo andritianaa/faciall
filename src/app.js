fdf2a36b03e5b01c0a084653073289a35f5a8470
andritianaa <andritianasteve@gmail.com>
2022-01-12 16:49:31 +0300

    multer import image code

--- src/app.js
+++ src/app.js
@@ -1,16 +1,50 @@
-const faceapi = require("face-api.js");
-const path = require('path');
-const canvas = require("canvas");
-const fs = require("fs");
-const express = require('express');
+const fs         = require("fs");
+const path       = require('path');
+const canvas     = require("canvas");
+const multer     = require('multer');
+const express    = require('express');
+const faceapi    = require("face-api.js");
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 const basePath = path.join(__dirname, '../public');
 app.use(express.static(basePath));
 
+
+// // parse application/json
+// app.use(bodyParser.json());
+
+  
+
+// // image upload code using multer
+// var storage = multer.diskStorage({
+//    destination: function (req, file, cb) {
+//       cb(null, 'uploads');
+//    },
+//    filename: function (req, file, cb) {
+//       cb(null, Date.now() + '-' + file.originalname);
+//    }
+// });
+
+// var upload = multer({ storage: storage });
+
+
+// app.post('/api/image-upload', upload.single('image'),(req, res) => {
+//   const image = req.image;
+//   res.send(apiResponse({message: 'File uploaded successfully.', image}));
+
+// });
+
+
+// function apiResponse(results){
+//     return JSON.stringify({"status": 200, "error": null, "response": results});
+// }
+
+// server listen 
 app.listen(port, () => {
     console.log("server started on port "+port);
 })
-//<>
 
+//<>
