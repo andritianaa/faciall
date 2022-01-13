851a239a1552464f3d136ca76e7d3fc5f1362d34
andritianaa <andritianasteve@gmail.com>
2022-01-13 23:27:50 +0300

    monkey patch

--- public/js/brouillon.js
+++ public/js/brouillon.js
@@ -1,40 +1,103 @@
-const video = document.getElementById("videoInput");
-const canvas = document.getElementById("canvas");
-const img = document.getElementById("img");
-const camera = document.getElementById("camera");
+const fs = require("fs");
+const path = require('path');
+const canvas = require("canvas");
+const multer = require('multer');
+const express = require('express');
+const faceapi = require("face-api.js");
+const bodyParser = require('body-parser');
+
+//definition glogal fileName
+//fileName est utilisé pour récuperer le nom du fichier uploadé, ici une image
+var fileName = 'test';
+const app = express();
+const port = process.env.PORT || 3000;
+
+const basePath = path.join(__dirname, '../public');
+app.use(express.static(basePath));
 
 
-setInterval(async ()=>{
-    const detections = await faceapi.detectAllFaces(camera).withFaceLandmarks();
-    //verification du nombre de visage
-    if(detections.length==0){
-        console.log("searching");
-    }else if(detections.length==1){
-        console.log(detections);
-        extractFaceFromBox(video, detections[0].detection.box);
-        video.pause();
-    }else{
-        console.log("Une personne à la fois");
-        
-    }
-},5000);
+// parse application/json
+app.use(bodyParser.json());
+
+// image upload code using multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+    fileName = file.originalname;
+  }
+});
+
+var upload = multer({
+  storage: storage
+});
+
+app.post('/', upload.single('image'), (req, res) => {
+  var image = req.image;
+
+  Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromDisk('public/models'),
+    faceapi.nets.faceLandmark68Net.loadFromDisk('public/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromDisk('public/models'),
+  ]).then(start);
+
+  async function start() {
+    console.log("insertion image a rechercher: ",fileName);
+    faceapi.env.monkeyPatch({canvas,image});
+    console.log("loading image");
+    image = await canvas.loadImage(`uploads/${fileName}`);
+    console.log("image loaded");
+    let fullFaceDescriptions = await faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptors();
+    fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions);
+
+  }
+
+  const labels = ["amy", "bernadette", "howard", "leonard", "penny", "raj", "sheldon", "stuart"];
+  const labeledFaceDescriptors = await Promise.all(
+    labels.map(async label => {
+      // fetch image data from urls and convert blob to HTMLImage element
+                img = fs.readFileSync(`public/images/${label}/${i}.png`);
+          faceapi.env.monkeyPatch({
+            canvas,
+            img
+          });
+          img = await canvas.loadImage(`public/images/${label}/${i}.png`);
+      
+      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+      const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+      
+      if (!fullFaceDescription) {
+        throw new Error(`no faces detected for ${label}`)
+      }
+      
+      const faceDescriptors = [fullFaceDescription.descriptor]
+      return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
+    })
+  )
+  
 
 
-// extracter une image depuis l'élément detection
-async function extractFaceFromBox(inputImage, box){ 
-    const regionsToExtract = [
-        new faceapi.Rect( box.x, box.y , box.width , box.height)
-    ];        
-    let faceImages = await faceapi.extractFaces(inputImage, regionsToExtract);
-    
-    if(faceImages.length == 0){
-        console.log('Face not found')
-    }
-    else
-    {
-        faceImages.forEach((cnv) => {
-            img.src = cnv.toDataURL();
-         });  
-    }   
+  res.send(apiResponse({
+    message: 'File uploaded successfully.',
+    image
+  }));
+});
+
+
+function apiResponse(results) {
+  return JSON.stringify({
+    "status": 200,
+    "error": null,
+    "response": results
+  });
 }
-// //<>
+
+// server listen 
+app.listen(port, () => {
+  console.log("server started on port " + port);
+})
+
+//<>
+
