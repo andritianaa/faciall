0061026fe72ef393e3e0e2ca50ec7e7e4c132334
andritianaa <andritianasteve@gmail.com>
2022-01-19 23:59:14 +0300

    externalize

--- src/app.js
+++ src/app.js
@@ -146,39 +146,6 @@ app.listen(port, () => {
 })
 
 
-//fonction mamadika image descriptor ho lasa JSON
-async function creatingDescriptorJSONfile() {
-  console.log("GO");
-  //monkey patch
-  const {
-    Canvas,
-    Image,
-    ImageData
-  } = canvas;
-  faceapi.env.monkeyPatch({
-    Canvas,
-    Image,
-    ImageData
-  })
-  //chargement des models
-  await faceapi.nets.faceRecognitionNet.loadFromDisk('public/models');
-  await faceapi.nets.faceLandmark68Net.loadFromDisk('public/models');
-  await faceapi.nets.ssdMobilenetv1.loadFromDisk('public/models');
-  //models chargés
-  console.log("models chargés");
-
-
-  img = await canvas.loadImage(`public/faces/females/rasta.jpg`);
-  console.log("imgReference loaded");
-
-  console.log("descripting");
-  const imgDescriptor = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-  //io imgDescriptor io no atsofoka anaty base de donnée
-  if (imgDescriptor) console.log("found face in imgDescriptor");
-  console.log(imgDescriptor);
-  //creation fichier json
-  fs.writeFile("./public/faces/females/rasta.json", JSON.stringify(imgDescriptor.descriptor), () => console.log("file writed"));
-}
 
 
 //<>
