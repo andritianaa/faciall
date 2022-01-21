fa07c75d5ec8ebf05b4453dd191b45b6ce9b5990
andritianaa <andritianasteve@gmail.com>
2022-01-21 23:33:29 +0300

    replace face models

--- public/js/brouillon.js
+++ public/js/brouillon.js
@@ -54,9 +54,9 @@ app.post('/', upload.single('image'), (req, res) => {
   async function start() {
 
     //chargement des models
-    await faceapi.nets.faceRecognitionNet.loadFromDisk('public/models');
-    await faceapi.nets.faceLandmark68Net.loadFromDisk('public/models');
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk('public/models');
+    await faceapi.nets.faceRecognitionNet.loadFromDisk('src/facial_recognition/models');
+    await faceapi.nets.faceLandmark68Net.loadFromDisk('src/facial_recognition/models');
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk('src/facial_recognition/models');
     //models chargés
 
 
