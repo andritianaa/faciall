91c39a6e3eac71267db1c97ff1505754306dea99
andritianaa <andritianasteve@gmail.com>
2022-01-24 23:30:18 +0300

    search filtered by genre done

--- src/facial_recognition/search.js
+++ src/facial_recognition/search.js
@@ -1,35 +1,39 @@
 const fs = require('fs');
 const faceapi = require("face-api.js");
 const compareFace = require('./compareFace.js');
+const { max } = require('@tensorflow/tfjs-core');
 
 async function search(imgDescriptor) {
     let found = false;
+    max = {
+        "id": null,
+        "distance": 1
+    };
     let id = 1;
     let referencePath;
+    genre = imgDescriptor.genre;
     while (!found) {
         for (let i = 1; i <= 3; i++) {
             referencePath = `./public/faces/${genre}s/${id}/${i}.json`
             let compareResult = compareFace.compareObjectJSON(imgDescriptor, referencePath);
-
-            //resultats
-            if (compareResult < 0.45) {
-                console.log("\nResults : Olona mitovy");
-                found = true;
-            } else if (compareResult > 0.45) {
-                console.log("\nResults : Olona samihafa ");
-            } else {
-                console.log("Sary mitovy");
-                found = true;
+            if (compareResult < max.distance){
+                max = {
+                    "id":id,
+                    "distance": compareResult
+                }
             }
+            if (compareResult < 0.3) {
+                found = true;
+                break;
+            }
+            
         }
         id++;
         //trouver nombre de dossier dans un dossier avec node.js
     }
     //ity id ity tadiavina ary amin'ny database
     if(found){
-        return {
-            'id':id
-        };
+        return max;
     }else{
         return 0;
     }
