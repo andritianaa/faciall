c26357ed9d064af728483ed155ec419d7cf3efb5
andritianaa <andritianasteve@gmail.com>
2022-02-05 22:27:34 +0300

    restart mongoDB

--- app.js
+++ app.js
@@ -1,45 +1,25 @@
-const fs = require('fs');
-const path = require('path');
-const multer = require('multer');
 const express = require('express');
-const bodyParser = require('body-parser');
+app = express();
+//<>
 
-require('./src/models/dbConfig');
 
-//init express
-const app = express();
-const port = process.env.PORT || 3000;
-const basePath = path.join(__dirname, '../public');
-app.use(express.static(basePath));
 
-//routes
 
-// parse application/json
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
 
-// image upload code using multer
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-    fileName = file.originalname;
-  }
-});
 
-var upload = multer({
-  storage: storage
-});
+
+
+
+
+
+
+
+
 
 
 
 // server listen 
+port = 3000;
 app.listen(port, () => {
   console.log("server started on port " + port);
 })
-
-//<>
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+
+
+
+const PORT = 3000;
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+})
+//<>
