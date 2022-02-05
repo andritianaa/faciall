c8091e964a6620ec4a9d527cbf219fe796bc9e78
andritianaa <andritianasteve@gmail.com>
2022-02-06 01:06:06 +0300

    server up

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-var Person = {
-  nom: "Andritiana",
-  prenom:"Steve",
-  age:20,
-  incAge:()=>{
-    this.age=age++;
-  },
-  getAge: ()=>{
-    console.log(this.age);
-    return this.age;
-  }
-  
-}
-console.log(Person.getAge());
-//<>
+const express = require("express");
+app = express();
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+  console.log(`Server starte on port ${PORT}`);
+})
