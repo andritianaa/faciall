4e35f7d805db6f797e96ad5c00dc60c4d35662ff
andritianaa <andritianasteve@gmail.com>
2022-02-07 22:59:07 +0300

    reinit project

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const personRoutes = require('./api/routes/persons');
 
-const app = express();
-app.use('/',(req,res,next)=>{
-    res.sendFile(__dirname+'/public/camera.html');
-})
-const PORT = process.env.PORT || 3000;
+//instatiate server
+const server = express();
 
-app.listen(PORT, ()=>{
+//routes
+server.use('/',(req,res,next)=>{
+    res.header('Content-Type','text/html');
+    res.status(200).send('<h1>connected to server</h1>');
+});
+
+
+
+//launch server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=>{
   console.log(`Server starte on port ${PORT}`);
 })
-//
+//<>
